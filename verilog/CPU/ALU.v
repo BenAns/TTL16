6c68509b66d9f35bad271f3d2b44f59dd6005c68
Ben Anscombe <benjamin.anscombe@hotmail.com>
2022-09-30 22:20:21 +0100

    Changed to 5-bit ALU opcodes, completed first implementation of ALU

--- verilog/CPU/ALU.v
+++ verilog/CPU/ALU.v
@@ -1,6 +1,6 @@
 module ALU(input [15:0] A,
 	input [15:0] B,
-	input [3:0] op,
+	input [4:0] op,
 	output [15:0] Q,
 	output overflow,
 	output less,
@@ -27,15 +27,28 @@ module ALU(input [15:0] A,
 	assign shiftCarry = (op[0] & B[0]) | (op[1] & B[15]);
 	assign rightShiftOut = {shiftCarry, B[15:1]};
 	assign leftShiftOut = {B[14:0], shiftCarry};
+	assign shiftOut = op[0] ? leftShiftOut : rightShiftOut;
 
 	// Get outputs
-	
+	assign Q = ~op[4] ? sumout :
+		~|op[3:2] ? andOut :
+		~op[3] & op[2] ? orOut :
+		&op[3:2] & ~|op[2:1] ? xorOut :
+		shiftOut;
+
+	assign overflow = (~|op[2:1] & cout) | (~op[1] & op[2] & B[0]);
+	assign zero = ~|Q;
+	assign isComparison = op[0] & op[1] & ~op[3];
+	assign comparisonXOR = op[2] & (A[15] ^ B[15]);
+	assign equal = zero & isComparison;
+	assign less = (comparisonXOR ^ Q[15]) & isComparison;
+	assign greater = ~equal & ~less & isComparison;
 endmodule
 
 module ALU_tests;
 	reg [15:0] A;
 	reg [15:0] B;
-	reg [3:0] op;
+	reg [4:0] op;
 
 	wire [15:0] Q;
 	wire overflow;
@@ -50,7 +63,8 @@ module ALU_tests;
 		$display("Testing ALU addition");
 		$display("Testing ALU subtraction");
 		$display("Testing ALU negation");
-		$display("Testing ALU comparison");
+		$display("Testing ALU signed comparison");
+		$display("Testing ALU unsigned comparison");
 		$display("Testing ALU AND");
 		$display("Testing ALU OR");
 		$display("Testing ALU XOR");
