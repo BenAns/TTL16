4cf2c9b9541666dae2d31ba35b3eec92ae8bbcd1
Ben Anscombe <benjamin.anscombe@hotmail.com>
2022-10-08 07:05:50 +0100

    Refactored ALU testing code, clarified aspects in ALU spec

--- verilog/CPU/ALU.v
+++ verilog/CPU/ALU.v
@@ -45,21 +45,23 @@ module ALU(input [15:0] A,
 	wire greater = ~equal & ~less & isComparison;
 endmodule
 
-`define OPCODE_NEG
-`define OPCODE_SCMP
-`define OPCODE_USMP
-`define OPCODE_AND
-`define OPCODE_OR
-`define OPCODE_XOR
-`define OPCODE_NOT
-`define OPCODE_ARS
-`define OPCODE_LRS
-`define OPCODE_LROT
-`define OPCODE_RROT
-
-`timescale 1us/10ns
-
 module ALU_tests;
+	integer STDERR = 32'h80000002;
+
+	integer OPCODE_ADD = 5'b00001;
+	integer OPCODE_SUB = 5'b00011;
+	integer OPCODE_NEG = 5'b00010;
+	integer OPCODE_SCMP = 5'b01011;
+	integer OPCODE_UCMP = 5'b01111;
+	integer OPCODE_AND = 5'b10000;
+	integer OPCODE_OR = 5'b10101;
+	integer OPCODE_XOR = 5'b11100;
+	integer OPCODE_NOT = 5'b10110;
+	integer OPCODE_ARS = 5'b11010;
+	integer OPCODE_LRS = 5'b11000;
+	integer OPCODE_LROT = 5'b11110;
+	integer OPCODE_RROT = 5'b11101;
+
 	reg [16:0] X;
 	reg [16:0] Y;
 	reg [4:0] op;
@@ -76,52 +78,69 @@ module ALU_tests;
 	wire [15:0] B = swappedInputs ? X[15:0] : Y[15:0];
 	
 	ALU testALU(A, B, op, Q, overflow, less, equal, greater, zero);
-
-	integer STDERR = 32'h80000002;
-	integer OPCODE_ADD = 5'b00001;
-	integer OPCODE_SUB = 5'b00011;
-	integer OPCODE_NEG = 5'b00010;
-	integer OPCODE_SCMP = 5'b01011;
-	integer OPCODE_UCMP = 5'b01111;
-	integer OPCODE_AND = 5'b10000;
-	integer OPCODE_OR = 5'b10101;
-	integer OPCODE_XOR = 5'b11100;
-	integer OPCODE_NOT = 5'b10110;
-	integer OPCODE_ARS = 5'b11010;
-	integer OPCODE_LRS = 5'b11000;
-	integer OPCODE_LROT = 5'b11110;
-	integer OPCODE_RROT = 5'b11101;
-
 	initial begin
 		$display("Starting main tests");
 		
 		$display("Testing ALU addition");
-		swappedInputs = 0;
-		#100;
-		testAdd();
-		swappedInputs = 1;
-		#100;
-		testAdd();
+		op = OPCODE_ADD;
+		addTestCase(0);
+		addTestCase(1);
+		addTestCase(2);
+		addTestCase(16'h4abc);
+		addTestCase(16'haabc);
+		addTestCase(16'hffff);
+		addTestCase(16'hfffe);
 
 		$display("Testing ALU subtraction");
-		swappedInputs = 0;
-		testSub();
+		op = OPCODE_SUB;
+		subTestCase(0);
+		subTestCase(1);
+		subTestCase(2);
+		subTestCase(16'hffff);
+		subTestCase(16'hfffe);
+		subTestCase(16'h2abc);
+		subTestCase(16'haabc);
 
 		$display("Testing ALU negation");
-		testNeg();
+		op = OPCODE_NEG;
+		negTestCase(0);
+		negTestCase(1);
+		negTestCase(10);
+		negTestCase(10000);
+		negTestCase(16'hfffe);
+		negTestCase(16'hffff);
 
 		$display("Testing ALU signed comparison");
-		testScmp();
+		op = OPCODE_SCMP;
+		cmpTestCase(1, 1);
+		cmpTestCase(1, -1);
+		cmpTestCase(1, 16'h7fff);
+		cmpTestCase(1, 16'h8000);
+		cmpTestCase(1, 100);
+		cmpTestCase(1, -100);
 
 		$display("Testing ALU unsigned comparison");
-		testUcmp();
+		op = OPCODE_UCMP;
+		cmpTestCase(0, 0);
+		cmpTestCase(0, 1);
+		cmpTestCase(0, 16'hffff);
+		cmpTestCase(0, 16'h7fff);
+		cmpTestCase(0, 16'h8000);
+		cmpTestCase(0, 16'h3aaa);
+		cmpTestCase(0, 16'haaaa);
 
 		$display("Testing ALU AND");
 		$display("Testing ALU OR");
 		$display("Testing ALU XOR");
 
 		$display("Testing ALU complement");
-		testNot();
+		op = OPCODE_NOT;
+		notTestCase(0);
+		notTestCase(1);
+		notTestCase(10);
+		notTestCase(10000);
+		notTestCase(16'hfffe);
+		notTestCase(16'hffff);
 
 		$display("Testing ALU arithmetic right shift");
 		$display("Testing ALU logical right shift");
@@ -129,8 +148,8 @@ module ALU_tests;
 		$display("Testing ALU right rotate");
 	end
 
-	task outputTest(input [255:0] testInfo, input incorrectQ, input incorrectOverflow, input incorrectLess, input incorrectEqual,
-		input incorrectGreater, input incorrectZero);
+	task outputTest(input [255:0] testInfo, input badQ, input badOverflow,
+		input badLess, input badEqual, input badGreater, input badZero);
 		begin
 			$fwrite(STDERR, {"Failed test: %s\n",
 				"A - %04x\nB - %04x\nop - %02x\n",
@@ -140,318 +159,197 @@ module ALU_tests;
 				testInfo, A, B, op, Q, overflow,
 				less, equal, greater, zero);
 
-			if(incorrectQ)        $fwrite(STDERR, " Q");
-			if(incorrectOverflow) $fwrite(STDERR, " overflow");
-			if(incorrectLess)     $fwrite(STDERR, " less");
-			if(incorrectEqual)    $fwrite(STDERR, " equal");
-			if(incorrectGreater)  $fwrite(STDERR, " greater");
-			if(incorrectZero)     $fwrite(STDERR, " zero");
+			if(badQ)        $fwrite(STDERR, " Q");
+			if(badOverflow) $fwrite(STDERR, " overflow");
+			if(badLess)     $fwrite(STDERR, " less");
+			if(badEqual)    $fwrite(STDERR, " equal");
+			if(badGreater)  $fwrite(STDERR, " greater");
+			if(badZero)     $fwrite(STDERR, " zero");
 			$fwrite(STDERR, "\n\n");
-
-			incorrectQ = 0;
-			incorrectOverflow = 0;
-			incorrectLess = 0;
-			incorrectEqual = 0;
-			incorrectGreater = 0;
-			incorrectZero = 0;
 		end
 	endtask
 
-	task checkTest(input [255:0] testInfo, input [15:0] expectedQ,
-		input expectedOverflow, input expectedLess, input expectedEqual,
-		input expectedGreater, input expectedZero);
+	task checkTest(input [255:0] testInfo, input [15:0] expQ, input expOverflow,
+		input expLess, input expEqual, input expGreater, input expZero);
 		reg error;
-		reg incorrectQ;
-		reg incorrectOverflow;
-		reg incorrectLess;
-		reg incorrectEqual;
-		reg incorrectGreater;
-		reg incorrectZero;
+		reg badQ;
+		reg badOverflow;
+		reg badLess;
+		reg badEqual;
+		reg badGreater;
+		reg badZero;
 		begin
 			error = 0;
-			incorrectQ = 0;
-			incorrectOverflow = 0;
-			incorrectLess = 0;
-			incorrectEqual = 0;
-			incorrectGreater = 0;
-			incorrectZero = 0;
+			badQ = 0;
+			badOverflow = 0;
+			badLess = 0;
+			badEqual = 0;
+			badGreater = 0;
+			badZero = 0;
 
-			#100;
+			#10;
 
-			if(^expectedQ !== 1'bx && expectedQ !== Q) begin
+			if(^expQ !== 1'bx && expQ != Q) begin
 				error = 1;
-				incorrectQ = 1;
+				badQ = 1;
 			end
-			if(expectedOverflow !== 1'bx && expectedOverflow !== overflow) begin
+			if(expOverflow != overflow) begin
 				error = 1;
-				incorrectOverflow = 1;
+				badOverflow = 1;
 			end
-			if(expectedLess !== 1'bx && expectedLess !== less) begin
+			if(expLess != less) begin
 				error = 1;
-				incorrectLess = 1;
+				badLess = 1;
 			end
-			if(expectedEqual !== 1'bx && expectedEqual !== equal) begin
+			if(expEqual != equal) begin
 				error = 1;
-				incorrectEqual = 1;
+				badEqual = 1;
 			end
-			if(expectedEqual !== 1'bx && expectedGreater !== greater) begin
+			if(expGreater != greater) begin
 				error = 1;
-				incorrectGreater = 1;
+				badGreater = 1;
 			end
-			if(expectedZero !== 1'bx && expectedZero !==  zero) begin
+			if(expZero != zero) begin
 				error = 1;
-				incorrectZero = 1;
+				badZero = 1;
 			end
 
 			if(error)
-				outputTest(testInfo, incorrectQ, incorrectOverflow,
-				incorrectLess, incorrectEqual, incorrectGreater,
-				incorrectZero);
+				outputTest(testInfo, badQ, badOverflow,
+				badLess, badEqual, badGreater,
+				badZero);
 		end
 	endtask
 
-	task testAdd;
-		begin
-			op = OPCODE_ADD;
-
-			// Test adding zero to every integer results in the same
-			// integer with no carry or comparison flags
-			X = 0;
-			for(Y = 0; Y < 17'h10000; Y++)
-				checkTest("Adding zero", Y[15:0], 0, 0, 0, 0,
-					Y == 0);
-			// Test adding 0xffff to every integer is equivalent
-			// to subtracting one and setting carry if necessary
-			X = 17'h0ffff;
-			for(Y = 0; Y < 17'h10000; Y++)
-				checkTest("Adding 0xffff", Y[15:0] - 1,
-					Y != 0, 0, 0, 0, Y == 1);
-
-			// Test adding one to every integer works correctly
-			X = 1; 
-			for(Y = 0; Y < 17'h10000; Y++)
-				checkTest("Adding one", Y[15:0] + 1,
-					Y == 17'h0ffff, 0, 0, 0,
-					Y == 17'h0ffff);
-		end
-	endtask
-
-	task testSub;
-		begin
-			op = OPCODE_SUB;
-
-			// Test subtracting zero from every integer results
-			// in the same integer
-			Y = 0;
-			for(X = 0; X < 17'h10000; X++)
-				checkTest("Subbing zero", X[15:0], 1, 0, 0,
-					0, X == 0);
-			
-			// Test zero subtract any number results in the
-			// negative of the number
-			X = 0;
-			for(Y = 0; Y < 17'h10000; Y++)
-				checkTest("Subbing from zero", -Y[15:0],
-					Y == 0, 0, 0, 0, Y == 0);
-			
-			// Test subtracting 0xffff from every integer is
-			// equivalent to adding one
-			Y = 17'h0ffff;
-			for(X = 0; X < 17'h10000; X++)
-				checkTest("Subbing 0xffff", X[15:0] + 1,
-					X == 17'h0ffff, 0, 0, 0, X == 17'h0ffff);
-
-			// Test subtracting from 0xffff works as should
-			// subtracting from -1
-			X = 17'h0ffff;
-			for(Y = 0; Y < 17'h10000; Y++)
-				checkTest("Subbing from 0xffff",
-					-1 - Y[15:0], 1, 0, 0, 0,
-					Y == 17'h0ffff);
-		end
-	endtask
-
-	task testNeg;
-		begin
-			op = OPCODE_NEG;
-
-			// Test every integer Y retrieves its correct -Y,
-			// varying X to make sure it is independent of X
-			X = 0;
-			negTestCase();
-
-			X = 1;
-			negTestCase();
-
-			X = 10;
-			negTestCase();
-
-			X = 10000;
-			negTestCase();
-
-			X = 17'h0fffe;
-			negTestCase();
-
-			X = 17'h0ffff;
-			negTestCase();
-		end
-	endtask
-
-	task negTestCase;
+	task addTestCase(input [15:0] fixedVal);
 		reg [1024:0] testName;
+		reg [16:0] sum;
 		begin
+			X = fixedVal;
+			$sformat(testName, "Adding %04x", fixedVal);
+
+			swappedInputs = 0;
 			for(Y = 0; Y < 17'h10000; Y++) begin
-				#1;
-				$sformat(testName, "Negating with X = %04x", X[15:0]);
-				checkTest(testName, -Y[15:0], 1'bx, 0, 0, 0, Y == 0);
-			end;
-		end
-	endtask
+				sum = X + Y;
 
-	task testScmp;
-		begin
-			op = OPCODE_SCMP;
+				swappedInputs = 0;
+				checkTest(testName, sum[15:0], sum[16],
+					1'bx, 1'bx, 1'bx, sum[15:0] == 0);
 
-			cmpTestCase(1, 0);
-			cmpTestCase(1, 1);
-			cmpTestCase(1, -1);
-			cmpTestCase(1, 16'h7fff);
-			cmpTestCase(1, 16'h8000);
-			cmpTestCase(1, 100);
-			cmpTestCase(1, -100);
-		end
-	endtask
-
-	task testUcmp;
-		begin
-			op = OPCODE_UCMP;
-
-			cmpTestCase(0, 0);
-			cmpTestCase(0, 1);
-			cmpTestCase(0, 16'hffff);
-			cmpTestCase(0, 16'h7fff);
-			cmpTestCase(0, 16'h8000);
-			cmpTestCase(0, 16'h3aaa);
-			cmpTestCase(0, 16'haaaa);
+				swappedInputs = 1;
+				checkTest(testName, sum[15:0], sum[16],
+					1'bx, 1'bx, 1'bx, sum[15:0] == 0);
 			end
+		end
+	endtask
+
+	task subTestCase(input [15:0] fixedVal);
+		reg [1023:0] testName;
+		reg [16:0] diff;
+		begin
+			swappedInputs = 0;
+			X = fixedVal;
+			$sformat(testName, "Subtraction with %04x", fixedVal);
+
+			for(Y = 0; Y < 17'h10000; Y++) begin
+				swappedInputs = 0;
+				diff = X - Y;
+				checkTest(testName, diff[15:0], 1'bx,
+					1'bx, 1'bx, 1'bx, diff[15:0] == 0);
+
+				swappedInputs = 1;
+				diff = Y - X;
+				checkTest(testName, diff[15:0], 1'bx,
+					1'bx, 1'bx, 1'bx, diff[15:0] == 0);
+			end
+		end
+	endtask
+
+	task negTestCase(input [15:0] fixedX);
+		reg [1023:0] testName;
+		begin
+			swappedInputs = 0;
+			X = fixedX;
+			$sformat(testName, "Negating with A = %04x", X[15:0]);
+
+			for(Y = 0; Y < 17'h10000; Y++)
+				checkTest(testName, -Y[15:0], 1'bx,
+					1'bx, 1'bx, 1'bx, Y == 0);
+		end
 	endtask
 
 	task cmpTestCase(input signedCmp, input [15:0] fixedVal);
-		reg [1024:0] testName;
-		reg sLess;
-		reg sEqual;
-		reg sGreater;
-		reg uLess;
-		reg uEqual;
-		reg uGreater;
+		reg [1023:0] testName;
 		reg less;
 		reg equal;
 		reg greater;
 		
 		begin
 			X = fixedVal;
-			
-			// With fixed A
-			swappedInputs = 0;
-			$sformat(testName, "%s comparison for A = %04x",
+			$sformat(testName, "%s comparison with %04x",
 				signedCmp ? "Signed" : "Unsigned", X[15:0]);
 
 			for(Y = 0; Y < 17'h10000; Y++) begin
-				#10;
-				sLess = $signed(A) < $signed(B);
-				sEqual = $signed(A) == $signed(B);
-				sGreater = $signed(A) > $signed(B);
+				less = signedCmp ? $signed(X[15:0]) < $signed(Y[15:0]) : X < Y;
+				equal = X == Y;
+				greater = ~less & ~equal;
 
-				uLess = $unsigned(A) < $unsigned(B);
-				uEqual = $unsigned(A) == $unsigned(B);
-				uGreater = $unsigned(A) > $unsigned(B);
-				
-				#10;
-				less = signedCmp ? sLess : uLess;
-				equal = signedCmp ? sEqual : uEqual;
-				greater = signedCmp ? sGreater : uGreater;
-
+				swappedInputs = 0;
 				checkTest(testName, 1'bx, 1'bx,
 					less, equal, greater, 1'bx);
-			end
-			
-			// With fixed B
-			swappedInputs = 1;
-			$sformat(testName, "%s comparison for B = %04x",
-				signedCmp ? "Signed" : "unsigned", X[15:0]);
 
-			for(Y = 0; Y < 17'h10000; Y++) begin
-				#10;
-				sLess = $signed(A) < $signed(B);
-				sEqual = $signed(A) == $signed(B);
-				sGreater = $signed(A) > $signed(B);
-
-				uLess = $unsigned(A) < $unsigned(B);
-				uEqual = $unsigned(A) == $unsigned(B);
-				uGreater = $unsigned(A) > $unsigned(B);
-
-				#10;
-				less = signedCmp ? sLess : uLess;
-				equal = signedCmp ? sEqual : uEqual;
-				greater = signedCmp ? sGreater : uGreater;
-
+				swappedInputs = 1;
 				checkTest(testName, 1'bx, 1'bx,
-					less, equal, greater, 1'bx);
+					greater, equal, less, 1'bx);
 			end
+		end
+	endtask
 
+	task andTestCase(input reg[15:0] fixedInput);
+		begin
+		end
+	endtask
+
+	task orTestCase(input reg[15:0] fixedInput);
+		begin
+		end
+	endtask
+
+	task xorTestCase(input reg[15:0] fixedInput);
+		begin
+		end
+	endtask
+
+	task notTestCase(input reg[15:0] fixedX);
+		reg [1023:0] testName;
+		begin
 			swappedInputs = 0;
+			X = fixedX;
+			$sformat(testName, "Complement with A = %04x", X[15:0]);
+
+			for(Y = 0; Y < 17'h10000; Y++)
+				checkTest(testName, ~Y[15:0], 1'bx,
+					1'bx, 1'bx, 1'bx, Y == 17'h0ffff);
 		end
 	endtask
 
-	task testAND;
+	task arsTestCase(input reg[15:0] fixedX);
 		begin
 		end
 	endtask
 
-	task testOR;
+	task lrsTestCase(input reg[15:0] fixedX);
 		begin
 		end
 	endtask
 
-	task testXOR;
+	task lrotTestCase(input reg[15:0] fixedX);
 		begin
 		end
 	endtask
 
-	task testNot;
+	task rrotTestCase(input reg[15:0] fixedX);
 		begin
-			op = OPCODE_NOT;
-
-			// Test every integer Y retrieves its correct ~Y,
-			// varying X to make sure it is independent of X
-			X = 0;
-			notTestCase();
-
-			X = 1;
-			notTestCase();
-
-			X = 10;
-			notTestCase();
-
-			X = 10000;
-			notTestCase();
-
-			X = 17'h0fffe;
-			notTestCase();
-
-			X = 17'h0ffff;
-			notTestCase();
-		end
-	endtask
-
-	task notTestCase;
-		reg [1024:0] testName;
-		begin
-			for(Y = 0; Y < 17'h10000; Y++) begin
-				#1;
-				$sformat(testName, "Complement with X = %04x", X[15:0]);
-				checkTest(testName, ~Y[15:0], 0, 0, 0, 0, Y == 17'h0ffff);
-			end;
 		end
 	endtask
 endmodule
